503d01206d02a74fd82125dbbd259b72fb6c1982
Donald Wu <yeukfei02@gmail.com>
2020-10-24 19:28:09 +0800

    refactor: 💡 add cors

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,13 @@ import resolvers from './resolvers/resolvers';
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 const port = process.env.PORT || 4000;
-const options = { port: port };
+const options = {
+  port: port,
+  cors: {
+    origin: '*',
+    methods: 'GET,PUT,PATCH,POST,DELETE',
+  },
+};
 server.start(options, () => {
   console.log('server is running on localhost:4000');
 });
