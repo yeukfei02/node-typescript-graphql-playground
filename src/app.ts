4dbd2493a1eecf98474238e6a294bf58603de121
Donald Wu <yeukfei02@gmail.com>
2020-11-10 15:02:57 +0800

    feat: 🎸 add cors and playground

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,13 @@ import { ApolloServer } from 'apollo-server';
 import typeDefs from './schema/typeDefs';
 import resolvers from './resolvers/resolvers';
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  cors: true,
+  introspection: true,
+  playground: true,
+});
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`server is running on ${url}`);
 });
