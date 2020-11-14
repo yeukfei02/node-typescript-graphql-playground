b4d1292bca73a8ea8432d0c3ede391ec7ebfcf3e
Donald Wu <yeukfei02@gmail.com>
2020-11-14 19:47:26 +0800

    feat: 🎸 add apollo-server-testing for test case

---
+++ src/test/testSuite.test.ts
@@ -0,0 +1,9 @@
+import { playerTest } from './player';
+import { teamTest } from './team';
+import { matchTest } from './match';
+
+describe('testSuite test case', () => {
+  playerTest();
+  teamTest();
+  matchTest();
+});
